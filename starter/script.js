a2fc62d0eafc68aad52ecbfab5b421c97da0995c
Marisa Ramon <mer0522@gmail.com>
2020-12-11 16:14:58 -0600

    change messages with different number inputs

--- starter/script.js
+++ starter/script.js
@@ -10,6 +10,10 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -17,5 +21,17 @@ document.querySelector('.check').addEventListener('click', function () {
   // 0 is falsey, but ! makes it true
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+    //when guess is correct
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' 🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
   }
 });
+
+// What happens when guess is correct?
+// What happens when guess is too low/high?
+
+// Start by defining secret number
